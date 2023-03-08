1f8e68af25cbde55b1293701b2f30489015e7ff5
Christopher Burrows <99897839+cbradiodrums@users.noreply.github.com>
2023-03-08 10:56:28 -0500

    Build Sprint 1 (minimum) - Develop a database interface class, Create random monster data with the MonsterLab library, Populate the database with at least 1000 monsters

--- app/data.py
+++ app/data.py
@@ -8,18 +8,46 @@ from pymongo import MongoClient
 
 
 class Database:
+    load_dotenv()
+    database = MongoClient(getenv('DB_URL'), tlsCAFile=where())["Bandersnatch"]
 
-    def seed(self, amount):
-        pass
+    def __init__(self, collection: str = 'MonsterTable'):
+        """ MongoDB: Instantiates the Database class with the collection method for further calls """
+        self.collection = self.database[collection]
+
+    def seed(self, amount: int = 10):
+        """ MongoDB: Randomly generates [amount] of monsters to insert into Database Collection """
+        monster_list = [Monster().to_dict() for _ in range(amount)]
+        self.collection.insert_many(monster_list)
 
     def reset(self):
-        pass
+        """ MongoDB: Empties the Database Collection of all documents """
+        return self.collection.delete_many({})
 
     def count(self) -> int:
-        pass
+        """ MongoDB: Counts the amount of documents in the Database Collection """
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        """ MongoDB: Converts the Database Collection into a pandas DataFrame """
+        item_details = self.collection.find()
+        items_df = DataFrame(item_details)
+        return items_df
 
     def html_table(self) -> str:
-        pass
+        """ MongoDB: Converts the Database Dataframe into an HTML Table """
+        item_details = self.collection.find()
+        items_df = DataFrame(item_details)
+        items_html = items_df.to_html()
+        return items_html
+
+
+# Instantiation / Test Drivers
+if __name__ == '__main__':
+
+    # Initially Populate DB with 1000 Monsters (if less than)
+    db = Database()
+    if db.count() < 1000:
+        difference = 1000 - db.count()
+        db.seed(amount=difference)
+    db.reset()  # Uncomment to reset table and test above!
