98c0f287d0211bfc05abddf132e223b205d3da56
dylanmhoke <dylan.m.hoke@outlook.com>
2024-01-18 14:33:40 -0800

    Creating seed functionality

--- app/data.py
+++ app/data.py
@@ -5,21 +5,42 @@ from dotenv import load_dotenv
 from MonsterLab import Monster
 from pandas import DataFrame
 from pymongo import MongoClient
+import random
+from datetime import datetime
 
 
 class Database:
+    load_dotenv()
+    database = MongoClient(getenv("CONNECTION_STR"), tlsCAFile=where())["Database"]
+    
+    def __init__(self, database, library = Monster):
+        #instantiate inputs for our database and Data source
+        self.database = database
+        self.library = library
 
-    def seed(self, amount):
-        pass
+    def seed(self, amount = 1000):
+        #correctly inserts the specified number of monsters into the
+        #collection
+        document = {}
+
+        for _ in range(amount):
+            for column_name in self.library.items():
+                document[column_name] = random.choice(Monster)
+
+        document["timestamp"] = datetime.utcnow()
+
+        self.database.insert_one(document)
 
     def reset(self):
-        pass
+        #correctly deletes all monsters from the collection
 
     def count(self) -> int:
-        pass
+        #correctly return sht enumber of monsters in the collection
 
     def dataframe(self) -> DataFrame:
-        pass
+        #correclty returns a DataFrame containing all monsters in the
+        #collection
 
     def html_table(self) -> str:
-        pass
+        #correctly returns an HTML table representation of the DataFrame or
+        #None if the collection is empty
