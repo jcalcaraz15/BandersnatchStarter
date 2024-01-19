a83430e23831558222350c1bd2e65edc6f3921f7
dylanmhoke <dylan.m.hoke@outlook.com>
2024-01-19 14:00:40 -0800

    Update

--- app/data.py
+++ app/data.py
@@ -2,44 +2,52 @@ from os import getenv
 from certifi import where
 from dotenv import load_dotenv
 from MonsterLab import Monster
+import pandas as pd
 from pandas import DataFrame
 from pymongo import MongoClient
-import random
-from datetime import datetime
 
 
 class Database:
     load_dotenv()
     database = MongoClient(getenv("CONNECTION_STR"), tlsCAFile=where())["Database"]
     
-    def __init__(self, database, library = Monster):
+    def __init__(self, collection: str):
         #instantiate inputs for our database and Data source
-        self.database = database
-        self.library = library
+        self.collection = self.database[collection]
 
     def seed(self, amount = 1000):
         #correctly inserts the specified number of monsters into the
         #collection
-        document = {}
-
-        for _ in range(amount):
-            for column_name in self.library.items():
-                document[column_name] = random.choice(Monster)
-
-        document["timestamp"] = datetime.utcnow()
-
-        self.database.insert_one(document)
+        #built in mongo function
+        return {f"{self.collection.insert_many([Monster().to_dict() for _ in range(amount)]).acknowledged}"}
+        
 
     def reset(self):
         #correctly deletes all monsters from the collection
-
+        #mongo functions we want to leave it an empty database delete collection
+        #built in mongo function
+        return self.collection.delete_many({})
+        
+    
     def count(self) -> int:
-        #correctly return sht enumber of monsters in the collection
-
+        #correctly returns the number of monsters in the collection
+        #mongo db count indices of dictionary or collection, count_documents self.collection.count_documents(Filter={})
+        #built in mongo function
+        return self.collection.count_documents({})
+    
     def dataframe(self) -> DataFrame:
-        #correclty returns a DataFrame containing all monsters in the
+        #correctly returns a DataFrame containing all monsters in the
         #collection
-
+        #return a pandas dataframe df = pd.DataFrame(self.collection.find)
+        #built in pandas function and then usage of find inside the pandas function which is a mongo function
+        return pd.DataFrame(list(self.collection.find({})))
+        
     def html_table(self) -> str:
         #correctly returns an HTML table representation of the DataFrame or
         #None if the collection is empty
+        #pandas function that converts to html dataframe.to_html() if count > 0 then return self.dataframe else return None
+        #usage of the other functions to produce a table, taps into the mongo table during usage of said functions.
+        if self.count() > 0:
+            return self.dataframe().to_html()
+        else:
+            return None
