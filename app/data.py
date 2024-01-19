54e8306b7bcbf75baa0fa9d1f300fe4ae0edb6f4
dylanmhoke <dylan.m.hoke@outlook.com>
2024-01-19 14:23:26 -0800

    Update

--- app/data.py
+++ app/data.py
@@ -9,44 +9,46 @@ from pymongo import MongoClient
 
 class Database:
     load_dotenv()
-    database = MongoClient(getenv("CONNECTION_STR"), tlsCAFile=where())["Database"]
+    database = MongoClient(getenv("CONNECTION_STR"),
+                           tlsCAFile=where())["Database"]
     
     def __init__(self, collection: str):
-        #instantiate inputs for our database and Data source
+        #establishes a connection between our database and the input
         self.collection = self.database[collection]
 
     def seed(self, amount = 1000):
-        #correctly inserts the specified number of monsters into the
-        #collection
-        #built in mongo function
-        return {f"{self.collection.insert_many([Monster().to_dict() for _ in range(amount)]).acknowledged}"}
-        
+        """correctly inserts the specified number of monsters into the
+        collection, uses a built in mongo function"""
+        return {f"{self.collection.insert_many([Monster().to_dict()
+                                                for _ in range(amount)])
+                                                .acknowledged}"}
 
     def reset(self):
-        #correctly deletes all monsters from the collection
-        #mongo functions we want to leave it an empty database delete collection
-        #built in mongo function
+        """correctly deletes all monsters from the collection
+        mongo functions we want to leave it an empty database delete
+        collection, built in mongo function"""
         return self.collection.delete_many({})
         
-    
     def count(self) -> int:
-        #correctly returns the number of monsters in the collection
-        #mongo db count indices of dictionary or collection, count_documents self.collection.count_documents(Filter={})
-        #built in mongo function
+        """correctly returns the number of monsters in the collection
+        mongo db count indices of dictionary or collection, count_documents
+        self.collection.count_documents(Filter={}) built in mongo function"""
         return self.collection.count_documents({})
     
     def dataframe(self) -> DataFrame:
-        #correctly returns a DataFrame containing all monsters in the
-        #collection
-        #return a pandas dataframe df = pd.DataFrame(self.collection.find)
-        #built in pandas function and then usage of find inside the pandas function which is a mongo function
+        """correctly returns a DataFrame containing all monsters in the
+        collection
+        return a pandas dataframe df = pd.DataFrame(self.collection.find)
+        built in pandas function and then usage of find inside the pandas
+        function which is a mongo function"""
         return pd.DataFrame(list(self.collection.find({})))
         
     def html_table(self) -> str:
-        #correctly returns an HTML table representation of the DataFrame or
-        #None if the collection is empty
-        #pandas function that converts to html dataframe.to_html() if count > 0 then return self.dataframe else return None
-        #usage of the other functions to produce a table, taps into the mongo table during usage of said functions.
+        """correctly returns an HTML table representation of the DataFrame or
+        None if the collection is empty pandas function that converts to html
+        dataframe.to_html() if count > 0 then return self.dataframe else
+        return None usage of the other functions to produce a table, taps
+        into the mongo table during usage of said functions."""
         if self.count() > 0:
             return self.dataframe().to_html()
         else:
