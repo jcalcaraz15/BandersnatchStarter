e0f989a0e3005b9a8e128067403655af75db996c
dylanmhoke <dylan.m.hoke@outlook.com>
2024-01-26 13:45:27 -0800

    Second Ticket Update

--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,19 @@
-from altair import Chart
+from altair import Chart, Tooltip
+from pandas import DataFrame
+from app.data import Database
 
+def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    graph = Chart(
+        df,
+        title=f"{x} by {y} for {target}",
+    ).mark_circle(size=100).encode(
+        x=x,
+        y=y,
+        color=target,
+        tooltip=Tooltip(df.columns.to_list())
+    )
+    return graph
 
-def chart(df, x, y, target) -> Chart:
-    pass
+if __name__ == '__main__':
+    collection_graph = Database("Collection")
+    collection_graph.seed()
--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.data import Database
 from app.graph import chart
 from app.machine import Machine
 
-SPRINT = 1
+SPRINT = 2
 APP = Flask(__name__)
 
 
@@ -39,7 +39,7 @@ def data():
 def view():
     if SPRINT < 2:
         return render_template("view.html")
-    db = Database()
+    db = Database("Collection")
     options = ["Level", "Health", "Energy", "Sanity", "Rarity"]
     x_axis = request.values.get("x_axis") or options[1]
     y_axis = request.values.get("y_axis") or options[2]
