14af0d2bd029b79b042d473f91705e45712bdc10
dylanmhoke <dylan.m.hoke@outlook.com>
2024-01-29 08:25:37 -0800

    Update

--- app/graph.py
+++ app/graph.py
@@ -3,9 +3,13 @@ from pandas import DataFrame
 from app.data import Database
 
 def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    """
+    Creates a chart based on our information from our data.py file.
+    """
     graph = Chart(
         df,
-        title=f"{x} by {y} for {target}"
+        title=f"{x} by {y} for {target}",
+        background = "gray"
     ).mark_circle(size=100).encode(
         x=x,
         y=y,
