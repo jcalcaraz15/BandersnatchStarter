33a6ffcdaec36ff29ceecc6bd4ee013663d661f8
UARKHAWG <uarkhawg@github.com>
2024-02-27 21:46:33 -0600

    updates

--- app/machine.py
+++ app/machine.py
@@ -12,36 +12,46 @@ class Machine:
     """
 
     def __init__(self, df: pd.DataFrame):
-        # Initialize attributes
+        """ Initialize attributes """
         self.name = "Random Forest Classifier"
-        self.timestamp = datetime.now()
-        # Preprocess data
-        target = df['Rarity']
-        features = df.drop(columns=['Rarity'])
-        # Train Model
-        self.model = RandomForestClassifier()
-        self.model.fit(features, target)
+        self.timestamp = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
 
-    # def retrain(self, model):
-    #     model = open
+        self.model = RandomForestClassifier()
+        X = df[['Level', 'Health', 'Energy', 'Sanity']]
+        y = df['Rarity']
+        self.model.fit(X, y)
+
 
     def __call__(self, pred_basis: pd.DataFrame):
-        # Predicts the rarity of items based on input features.
+        """ Predicts the rarity of items based on input features. """
         prediction, *_ = self.model.predict(pred_basis)
         confidence, *_ = self.model.predict_proba(pred_basis)
         return prediction, max(confidence)
 
     def save(self, filepath: str):
-        # Saves the model to a file using joblib.
+        """ Saves the model to a file using joblib. """
         joblib.dump(self, filepath)
 
     @staticmethod
     def open(filepath: str):
-        # Loads a saved model from a file using joblib.
+        """ Loads a saved model from a file using joblib. """
         model = joblib.load(filepath)
         return model
 
     def info(self):
-        # Returns model name and timestamp
+        """ Returns model name and timestamp """
         timestamp = datetime.now().strftime('%Y-%m-%d %I:%M:%S %p')
         return f"Base Model: {self.name} <br> Timestamp: {timestamp}"
+
+
+if __name__ == '__main__':
+    from data import MongoDB
+
+    db = MongoDB('Collection')
+    df = db.dataframe()
+    machine = Machine(df)
+    machine.save('random_forest_model.joblib')
+    loaded_model = Machine.open('random_forest_model.joblib')
+    print(loaded_model)
+
+    
